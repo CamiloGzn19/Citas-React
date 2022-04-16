a984fa89b582a8de0d6f726b4583583d71a5f1f9
Camilo Garzón <Camilogzn19@gmail.com>
2022-04-16 17:24:35 -0500

    first advance

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Cita from "./components/Cita";
+import Formulario from "./components/Formulario";
 
 function App() {
+  // Arreglo de citas
+  const [citas, guardarCitas] = useState([]);
+
+  // Función que tome las citas actuales y agrege la nueva
+  const crearCita = (cita) => {
+    guardarCitas([...citas, cita]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Administrador de pacientes</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario crearCita={crearCita} />
+          </div>
+          <div className="one-half column">
+            <h2>Administra tus citas</h2>
+            {citas.map((cita) => (
+              <Cita key={cita.id} cita={cita} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
